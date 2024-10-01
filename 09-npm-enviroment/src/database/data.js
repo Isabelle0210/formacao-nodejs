ff55420fafb3c9831a7a32017ffe83e9ff4941ef
Isabelle0210 <isabelleisa646@gmail.com>
2024-10-01 14:58:40 -0300

    .

--- 09-npm-enviroment/src/database/data.js
+++ 09-npm-enviroment/src/database/data.js
@@ -1,5 +1,6 @@
 async function connectToDatabase(user, password) {
   if (
+    // Verifica se o usuário e senha passados são iguais as variáveis de ambiente do arquivo .env 
     user === process.env.USERDATABASE &&
     password === process.env.PASSWORDDATABASE
   ) {
